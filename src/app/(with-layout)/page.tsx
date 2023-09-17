f3d15445ec107d8ded9e1db4c68eb380a21cca4e
Alamin <alamin2.mas@gmail.com>
2023-09-17 21:45:33 +0600

    Image component explore

--- src/app/(with-layout)/page.tsx
+++ src/app/(with-layout)/page.tsx
@@ -1,7 +1,12 @@
+import image1 from "@/assets/beautiful-landscapes-wallpapers-11.jpg";
+// import image2 from "@/assets/flowers_pink_petals_90732_1920x1080.jpg";
+import Image from "next/image";
+
 export default function Home() {
    return (
       <main>
          <h2>Main Page</h2>
+         <Image width={500} src={image1} alt="" />
       </main>
    );
 }
